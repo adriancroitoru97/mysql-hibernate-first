7861053f3dafa0b97bbfa60c7a81f24fc02e3b13
Adrian Croitoru <adrian.croitoru.97@gmail.com>
2022-09-06 15:59:23 +0300

    UUID problem solved & configured ModelMapper.

--- src/main/java/com/sqlproject/demo/config/ModelMapperConfig.java
+++ src/main/java/com/sqlproject/demo/config/ModelMapperConfig.java
@@ -1,7 +1,21 @@
 package com.sqlproject.demo.config;
 
+import com.sqlproject.demo.dto.UserDTO;
+import com.sqlproject.demo.model.User;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeMap;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        TypeMap<User, UserDTO> propertyMapper = modelMapper.createTypeMap(User.class, UserDTO.class);
+        propertyMapper.addMappings(mapper -> mapper.skip(UserDTO::setId));
+
+        return modelMapper;
+    }
 }
--- src/main/java/com/sqlproject/demo/repository/UserRepository.java
+++ src/main/java/com/sqlproject/demo/repository/UserRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<List<User>> findByEmail(String email);
     Optional<List<User>> findByEmailAndAge(String email, Integer age);
 }
